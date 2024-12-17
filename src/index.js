20a70d8d6c1bc7b0518629f9e80191adb3c15410
elux-io <133917886+elux-io@users.noreply.github.com>
2024-12-18 00:02:39 +0100

    lines

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { getBadge, getCoverageBadge } = require('./badges.js')
+const { getBadge, getCoverageBadge, getLinesBadge } = require('./badges.js')
 const opentype = require('opentype.js')
 const fs = require('fs')
 const path = require('path')
@@ -10,6 +10,7 @@ const path = require('path')
         const status = core.getInput('status')
         const os = core.getInput('os')
         const coverage = core.getInput('coverage')
+        const linesData = core.getInput('lines_data')
 
         const dark = theme === 'dark'
         let svg
@@ -19,8 +20,18 @@ const path = require('path')
         } else {
             const fontPath = path.normalize(path.join(__dirname, '..', 'fonts', 'Outfit-Medium.ttf'))
             const buffer = (await fs.promises.readFile(fontPath)).buffer
-            const font = opentype.parse(buffer)
-            svg = getCoverageBadge(font, coverage, dark)
+            const fontMedium = opentype.parse(buffer)
+
+            if (coverage !== '') {
+                svg = getCoverageBadge(fontMedium, coverage, dark)
+            } else {
+                const fontPath = path.normalize(path.join(__dirname, '..', 'fonts', 'Outfit-Regular.ttf'))
+                const buffer = (await fs.promises.readFile(fontPath)).buffer
+                const fontRegular = opentype.parse(buffer)
+                const data = JSON.parse(linesData)
+
+                svg = getLinesBadge(fontMedium, fontRegular, data, dark)
+            }
         }
 
         core.setOutput('svg', svg)
